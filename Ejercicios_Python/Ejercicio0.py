8344e095c7e65029f8cb57e219adce656e0a68fb
Solll Monsalve <132629506+Sebxzttian@users.noreply.github.com>
2024-07-24 21:37:15 -0500

    se creo otra pagina

---
+++ Ejercicios_Python/Ejercicio0.py
@@ -0,0 +1,141 @@
+#help('keywords')
+#num1=type(9)
+#print(num1)
+
+
+#Entero=type(8)
+#Decimal=type(8.1)
+#print(Entero)
+#print(Decimal)
+
+
+#space_odyssey=2001
+#print(space_odyssey)
+
+
+#print(type('Good night & Good luck'))
+
+
+#print(type(True))
+
+
+#result=type(10*3.0)
+#print(result)
+
+
+#num1=10
+#num2=3
+#result=num1+num2
+#result=num1-num2
+#result=num1*num2
+#result=num1/num2
+#result=num1//num2
+#result=num1%num2
+#result=num1**num2
+#print(result)
+
+
+#total_cars = 100
+#sold_cars = 20
+#total_cars = total_cars - sold_cars
+#print(total_cars)
+
+
+#total_cars = 100
+#sold_cars = 20
+#total_cars -= sold_cars
+#print(total_cars)
+
+
+#num1=10
+#num2=3
+#result=num1/num2
+#print(round(result, 2))
+
+
+#num1=10
+#num2=3
+#result=num1/num2
+#result=int(result)
+#print(str(num1))
+
+
+#'''msg = 'Primera línea\nSegunda línea\nTercera línea'
+#print(msg)
+
+#msg = 'Valor = \t40'
+#print(msg)
+
+#msg = 'Necesitamos \'escapar\' la comilla simple'
+#print(msg)
+
+#msg = 'Capítulo \\ Sección \\ Encabezado'
+#print(msg)'''
+
+
+#msg1 = '¿Sabes por qué estoy acá?'
+#msg2 = 'Porque me apasiona'
+
+#print(msg1, msg2)
+#print(msg1, msg2, sep='|')
+#print(msg2, end='!!')
+
+
+#name = input('Introduzca su nombre: ')
+#print("Hola mucho gusto " + ( name))
+
+#age = input('Introduzca su edad: ')
+#print("Hola que interesante saber tu edad que es " + (age))
+
+
+#num1 = input('Ingrese el primer número: ')
+#num2 = input('Ingrese el segundo número: ')
+
+#suma = int(num1) + int(num2)
+#resta = int(num1) - int(num2)
+#multiplicacion = int(num1) * int(num2)
+#division = int(num1) / int(num2)
+
+#print('La suma de los dos números es: ' + str(suma))
+#print('La resta de los dos números es: ' + str(resta))
+#print('La multiplicación de los dos números es: ' + str(multiplicacion))
+#print('La división de los dos números es: ' + str(division))
+
+
+#value1 = int(input('Enter first number: '))
+#value2 = int(input('Enter second number: '))
+
+#result = value1 + value2
+#print(value1, value2, sep='+', end='=')
+#print(result)
+
+#result = value1 - value2
+#print(value1, value2, sep='-', end='=')
+#print(result)
+
+#result = value1 * value2
+#print(value1, value2, sep='*', end='=')
+#print(result)
+
+#result = value1 / value2
+#print(value1, value2, sep='/', end='=')
+#print(result)
+
+
+#reaction = 'Wow'
+#print(reaction * 4)
+
+
+#sentence = 'Hola, mundo'
+#print(sentence[0], sentence[1], sentence[2], sentence[3], sentence[4])
+
+
+#proverb = 'Lo cortés no quita lo valiente'
+#print(len(proverb))
+
+
+name = 'Elon Musk'
+age = 49
+fortune = 43_300
+
+print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
