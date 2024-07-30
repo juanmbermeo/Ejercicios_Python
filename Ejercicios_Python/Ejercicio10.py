2c9e4b1b17b1326333668f2fd59661861f0a8ae4
juanmbermeo <bermeojuanmanuel1@gmail.com>
2024-07-30 15:38:26 -0500

    Se añadio ejercicio10

---
+++ Ejercicios_Python/Ejercicio10.py
@@ -0,0 +1,10 @@
+#Diferencias entre Python y JavaScript
+
+#JavaScript
+#Es un lenguaje de programación principalmente utilizado para crear páginas web interactivas. 
+#Es el lenguaje "nativo" de los navegadores web y se ejecuta directamente en ellos.
+
+#Python
+#Es un lenguaje de programación de propósito general, utilizado en una amplia variedad de aplicaciones, 
+#como desarrollo web, ciencia de datos, inteligencia artificial y automatización. 
+#Es conocido por su sintaxis sencilla y fácil de leer.
