2e7fcb78f7ed5a4f777670a581edbdd4fe787a7a
juanmbermeo <bermeojuanmanuel1@gmail.com>
2024-07-31 22:04:47 -0500

    Listas

---
+++ Ejercicios_Python/Listas.py
@@ -0,0 +1,27 @@
+#Operaciones con listas
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.append('Atún')
+
+print(shopping)'''
+
+#Creando desde vacío
+'''even_numbers = []
+
+for i in range(20):
+    if i % 2 == 1:
+        even_numbers.append(i)
+
+print(even_numbers)'''
+
+#Añadir en cualquier posición de una lista
+shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.insert(1, 'Jamón')
+
+shopping
+['Agua', 'Jamón', 'Huevos', 'Aceite']
+
+shopping.insert(3, 'Queso')
+
+print(shopping)
